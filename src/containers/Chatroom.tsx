93fcc6cae2f4aa5bc71b8fb2928685dc64be0318
Gabriel <gabrielbuxo1@gmail.com>
2023-09-21 16:50:26 +0200

    added tailwind and scrolls to bottom when a new message arrives

--- src/containers/Chatroom.tsx
+++ src/containers/Chatroom.tsx
@@ -2,7 +2,7 @@ import { useCollection } from 'react-firebase-hooks/firestore'
 import { Firestore, addDoc, collection, limit, orderBy, query, serverTimestamp } from "firebase/firestore"
 import Message from '../components/Message'
 import Loading from '../components/Loading'
-import { FormEvent, useState } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 import { getAuth } from 'firebase/auth'
 
 
@@ -12,6 +12,8 @@ export default function Chatroom({db}: {db: Firestore}) {
 
     const [snapshot, loading] = useCollection(q)
     const [formValue, setFormValue] = useState('')
+
+    const dummy = useRef()
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
@@ -26,12 +28,17 @@ export default function Chatroom({db}: {db: Firestore}) {
         })
 
         setFormValue('')
+
+        dummy.current!.scrollIntoView({ behavior: "smooth" })
     }
 
     return (<>
-    <h1>Messages</h1>
-        {loading && <Loading />}
-        {snapshot && snapshot.docs.map(msg => <Message key={msg.id} message={msg.data()}/>)}
+        <h1>Messages</h1>
+        <main>
+            {loading && <Loading />}
+            {snapshot && snapshot.docs.map(msg => <Message key={msg.id} message={msg.data()}/>)}
+            <div ref={dummy}></div>
+        </main>
         <form onSubmit={handleSubmit}>
             <input value={formValue} onChange={e => setFormValue(e.target.value)} type='text'/>
             <button type='submit'>Submit</button>
